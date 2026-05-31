ad0883939fe48b5bed4d5f9efb853550cfc62e8f
jordan-chris191 <jordanchrisangelu@gmail.com>
2026-05-31 16:16:46 +0800

    updated bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,10 +4,10 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-if (method_exists(Dotenv::class, 'bootEnv')) {
+if (file_exists(dirname(__DIR__).'/.env')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
 
-if ($_SERVER['APP_DEBUG']) {
+if (($_SERVER['APP_DEBUG'] ?? false) === true) {
     umask(0000);
 }
