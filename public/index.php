520bff6845106fd4c3caef1fdc1a4e6874d29959
jordan-chris191 <jordanchrisangelu@gmail.com>
2026-05-31 16:29:39 +0800

    updated index.php

--- public/index.php
+++ public/index.php
@@ -5,5 +5,21 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+
+    // Force environment from real server variables (Railway, Docker, etc.)
+    $env = $_SERVER['APP_ENV']
+        ?? $_ENV['APP_ENV']
+        ?? getenv('APP_ENV')
+        ?? 'prod';
+
+    // Force debug flag safely
+    $debug = filter_var(
+        $_SERVER['APP_DEBUG']
+        ?? $_ENV['APP_DEBUG']
+        ?? getenv('APP_DEBUG')
+        ?? false,
+        FILTER_VALIDATE_BOOLEAN
+    );
+
+    return new Kernel($env, $debug);
 };
