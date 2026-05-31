405b3c6a4f5a19c587f227cbb828f0bb92cee4b8
jordan-chris191 <jordanchrisangelu@gmail.com>
2026-05-31 16:38:42 +0800

    updated bootstrap 2

--- public/index.php
+++ public/index.php
@@ -6,20 +6,11 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
 
-    // Force environment from real server variables (Railway, Docker, etc.)
-    $env = $_SERVER['APP_ENV']
-        ?? $_ENV['APP_ENV']
-        ?? getenv('APP_ENV')
-        ?? 'prod';
-
-    // Force debug flag safely
-    $debug = filter_var(
-        $_SERVER['APP_DEBUG']
-        ?? $_ENV['APP_DEBUG']
-        ?? getenv('APP_DEBUG')
-        ?? false,
-        FILTER_VALIDATE_BOOLEAN
+    return new Kernel(
+        $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? getenv('APP_ENV') ?? 'prod',
+        filter_var(
+            $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? getenv('APP_DEBUG') ?? false,
+            FILTER_VALIDATE_BOOLEAN
+        )
     );
-
-    return new Kernel($env, $debug);
 };
